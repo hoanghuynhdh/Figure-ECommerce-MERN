b721925efa7685f72142e5ad7afbbf87343a92ac
hoanghuynhdh <kickoffmanvn@gmail.com>
2021-03-20 22:10:49 +0700

    update package

--- backend/server.js
+++ backend/server.js
@@ -24,9 +24,9 @@ app.use("/api/orders", orderRouter);
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 app.use(express.static(path.join(__dirname, "/frontend/build")));
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
-);
+// app.get("*", (req, res) =>
+//   res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+// );
 app.get("/", (req, res) => {
   res.send("Server is ready");
 });
