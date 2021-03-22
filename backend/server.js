3da999a79ed7ed7d9a4c7df9be78e1bae3095e87
hoanghuynhdh <kickoffmanvn@gmail.com>
2021-03-22 22:06:22 +0700

    heroku 8

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-mongoose.connect(process.env.MONGODB_URL || "mongodb+srv://hoanghuynhdh99:0898124973@mern.wituw.mongodb.net/test", {
+mongoose.connect(process.env.MONGODB_URL || "mongodb+srv://hoanghuynhdh99:0898124973@mern.wituw.mongodb.net/figure?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
